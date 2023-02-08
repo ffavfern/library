1ab2701e14d6941d7bb35b30d0e5f20f33eb64bf
ffavfrn <ffern27744@gmail.com>
2023-02-08 20:11:51 +0700

    first commit

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+// Add functionality to the buttons using event listeners
+const searchBtn = document.querySelector('.search-btn');
+searchBtn.addEventListener('click', function() {
+  // Search functionality
+});
+
+const bookshelfBtn = document.querySelector('.bookshelf-btn');
+bookshelfBtn.addEventListener('click', function() {
+  // Bookshelf functionality
+});
+
+const libraryRoomBtn = document.querySelector('.library-room-btn');
+libraryRoomBtn.addEventListener('click', function() {
+  // Library room functionality
+});
+
+const categoryBtn = document.querySelector('.category-btn');
+categoryBtn.addEventListener('click', function() {
+  // Category functionality
+});
+
+const profileBtn = document.querySelector('.profile-btn');
+profileBtn.addEventListener('click', function() {
+  // Profile functionality
+});
---
+++ index.php
@@ -0,0 +1,116 @@
+<?php include 'header.php' ?>
+<nav></nav>
+<div class="container content">
+    <div class="new-arrival">
+
+        <h1 class="title col-8">New Arrival</h1>
+
+
+        <div class="row">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="./assets/img/cover-book.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">ความลับที่คนอ่านหนังสือเท่านั้นจะรู้</h5>
+                        <p class="card-text">เทคนิคอ่านหนังสือให้เข้าใจได้ลึกซึ่งกว่าเดิม ที่จะช่วยเพิ่มพลังความคิด พลังความรู้ และทำให้ชีวิตคุณมีวิสัยทัศน์ที่ล้ำลึกมากขึ้น
+                        </p>
+                        <a href="#" class="btn btn-danger">ยืม</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="./assets/img/cover-book.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">ความลับที่คนอ่านหนังสือเท่านั้นจะรู้</h5>
+                        <p class="card-text">เทคนิคอ่านหนังสือให้เข้าใจได้ลึกซึ่งกว่าเดิม ที่จะช่วยเพิ่มพลังความคิด พลังความรู้ และทำให้ชีวิตคุณมีวิสัยทัศน์ที่ล้ำลึกมากขึ้น
+                        </p>
+                        <a href="#" class="btn btn-danger">ยืม</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="./assets/img/cover-book.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">ความลับที่คนอ่านหนังสือเท่านั้นจะรู้</h5>
+                        <p class="card-text">เทคนิคอ่านหนังสือให้เข้าใจได้ลึกซึ่งกว่าเดิม ที่จะช่วยเพิ่มพลังความคิด พลังความรู้ และทำให้ชีวิตคุณมีวิสัยทัศน์ที่ล้ำลึกมากขึ้น
+                        </p>
+                        <a href="#" class="btn btn-danger">ยืม</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="highlight-book">
+        <h1 class="title hl-book">Highlight Book</h1>
+        <div class="row">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="./assets/img/cover-book.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">ความลับที่คนอ่านหนังสือเท่านั้นจะรู้</h5>
+                        <p class="card-text">เทคนิคอ่านหนังสือให้เข้าใจได้ลึกซึ่งกว่าเดิม ที่จะช่วยเพิ่มพลังความคิด พลังความรู้ และทำให้ชีวิตคุณมีวิสัยทัศน์ที่ล้ำลึกมากขึ้น
+                        </p>
+                        <a href="#" class="btn btn-danger">ยืม</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="./assets/img/cover-book.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">ความลับที่คนอ่านหนังสือเท่านั้นจะรู้</h5>
+                        <p class="card-text">เทคนิคอ่านหนังสือให้เข้าใจได้ลึกซึ่งกว่าเดิม ที่จะช่วยเพิ่มพลังความคิด พลังความรู้ และทำให้ชีวิตคุณมีวิสัยทัศน์ที่ล้ำลึกมากขึ้น
+                        </p>
+                        <a href="#" class="btn btn-danger">ยืม</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="./assets/img/cover-book.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">ความลับที่คนอ่านหนังสือเท่านั้นจะรู้</h5>
+                        <p class="card-text">เทคนิคอ่านหนังสือให้เข้าใจได้ลึกซึ่งกว่าเดิม ที่จะช่วยเพิ่มพลังความคิด พลังความรู้ และทำให้ชีวิตคุณมีวิสัยทัศน์ที่ล้ำลึกมากขึ้น
+                        </p>
+                        <a href="#" class="btn btn-danger">ยืม</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<footer class="library-footer">
+    <div class="footer-content">
+        <div class="wrap">
+            <img src="./assets/icon/search.png" class="icon-search" alt="search">
+            <button class="search-btn">Search</button>
+        </div>
+        <div class="wrap">
+            <img src="./assets/icon/bookshelf.png" class="icon-search" alt="search">
+            <button class="search-btn">Bookshelf</button>
+        </div>
+        <div class="wrap">
+            <img src="./assets/icon/library room.png" class="icon-search" alt="search">
+            <button class="search-btn">Library Room</button>
+        </div>
+        <div class="wrap">
+            <img src="./assets/icon/category.png" class="icon-search" alt="search">
+            <button class="search-btn">Category</button>
+        </div>
+        <div class="wrap">
+            <img src="./assets/icon/profile.png" class="icon-search" alt="search">
+            <button class="search-btn">Profile</button>
+        </div>
+        <div class="background"></div>
+
+    </div>
+</footer>
+
+
+</body>
+
+</html>
